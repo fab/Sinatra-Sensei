02d70549b07062c7000c0832551cc0a9697cb1fe
Apprentice <apprentice@devbootcamp.com>
2013-04-25 12:23:55 -0700

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 post1 = Post.create(title: 'This is my first post!', 
-                    body: 'Hello world. This is my blog! My first post is a short one.')
+                    body: 'Hello world. This is my blog! My first post is a short one.',
+                    user_id: 1)
 
 user1 = User.create(first_name: 'Fab', last_name: 'Mackojc', 
                     email: 'fab@gmail.com', password: 'hello')
 
 tag1 = Tag.create(name: 'personal')
 
-comment1 = Comment.create(content: 'This is my first comment!')
+comment1 = Comment.create(content: 'This is my first comment!', user_id: 1, post_id: 1)
